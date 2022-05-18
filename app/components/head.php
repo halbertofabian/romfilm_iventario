ef60cb45a8448b287a1b0673c8ba4487cc7e858f
Your Name <victor081196>
2022-05-18 11:05:05 -0500

    Romfilm

--- app/components/head.php
+++ app/components/head.php
@@ -17,9 +17,9 @@
     <!--    Favicons-->
     <!-- ===============================================-->
     <link rel="apple-touch-icon" sizes="180x180" href="<?= HTTP_HOST . 'app/' ?>assets/img/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= HTTP_HOST . 'app/' ?>assets/img/romfilm/logo.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= HTTP_HOST . 'app/' ?>assets/img/romfilm/logo.png">
-    <link rel="shortcut icon" type="image/x-icon" href="<?= HTTP_HOST . 'app/' ?>assets/img/romfilm/logo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= HTTP_HOST . 'app/' ?>assets/img/romfilm/logo_white.jpeg">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= HTTP_HOST . 'app/' ?>assets/img/romfilm/logo_white.jpeg">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= HTTP_HOST . 'app/' ?>assets/img/romfilm/logo_white.jpeg">
     <link rel="manifest" href="<?= HTTP_HOST . 'app/' ?>assets/img/favicons/manifest.json">
     <meta name="msapplication-TileImage" content="<?= HTTP_HOST . 'app/' ?>assets/img/favicons/mstile-150x150.png">
     <meta name="theme-color" content="#ffffff">
